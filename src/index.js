6361a3bf8d58c60ae049d0493fef098c9537851e
chydiwe <magiceco71@gmail.com>
2018-12-04 22:12:36 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Table from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Start = () => <div className='StartMenu'>
+    <Link to='/table?rows=1000'>
+        <button type='button'>BIG SIZE</button>
+    </Link>
+    <Link to='/table?rows=32'>
+        <button type='button'>small size</button>
+    </Link>
+</div>
+const App = () =>
+    <Switch>
+        <Route exact path='/' component={Start}/>
+        <Route path='/table' component={Table}/>
+    </Switch>
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
